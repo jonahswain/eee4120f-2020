4cf5b2db6b917f7cfcaedbef596d4f6170740d88
Jonah Swain <jonahswain@gmail.com>
2020-04-28 13:59:45 +0200

    [add] Clock division module

---
+++ Prac 4/Verilog/ClkDiv.v
@@ -0,0 +1,17 @@
+// EEE4120F (HPES)
+// Clock division module
+// Author(s): Jonah Swain [SWNJON003]
+
+module ClkDiv #(parameter divisor=100000) (input clk, output reg divclk);
+    localparam cnt_width = $clog2(divisor);
+    localparam cnt_max = divisor - 1;
+    localparam cnt_halfmax = divisor/2;
+
+    reg [cnt_width-1:0]cnt = 0;
+
+    always @(posedge clk)
+    begin
+        cnt <= (cnt > cnt_max)? 0:cnt+1;
+        divclk <= (cnt < cnt_halfmax)? 1'b0:1'b1;
+    end
+endmodule

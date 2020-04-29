2220513e835c5df8de040f683ac66f41bf24f0df
Jonah Swain <jonahswain@gmail.com>
2020-04-29 11:05:43 +0200

    [add] PWM module

--- Prac 4/Verilog/PWM.v
+++ Prac 4/Verilog/PWM.v
@@ -1,31 +1,16 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    09:57:30 05/30/2017 
-// Design Name: 
-// Module Name:    PWM 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module PWM(
-    input clk,			//input clock
-    input [7:0] pwm_in, 
-    output reg pwm_out 	//output of PWM	
-);
-	
-always @(posedge clk) begin
-    //Write your implementation here	
-end
-	
+// EEE4120F (HPES)
+// PWM module
+// Author(s): Jonah Swain [SWNJON003]
+
+module PWM #(parameter resolution_bits=8) (input clk, input [resolution_bits-1:0] pwm_in, output reg pwm_out);
+    reg [resolution_bits-1:0] cntr = 0;
+    reg [resolution_bits-1:0] pwmr = 0;
+
+    always @(posedge clk)
+    begin
+        pwmr <= pwm_in;
+        cntr <= cntr + 1;
+        pwm_out <= (pwmr > cntr)? 1'b1:1'b0;
+    end
 endmodule

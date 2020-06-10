31ef41c26827119b6e8391b1560dde75124e9c80
Jonah Swain <jonahswain@gmail.com>
2020-06-10 17:26:41 +0200

    [edit] TB_FP_MUL -> TB_FP_MAC

--- YODA/HDL/testbenches/TB_FP_MAC.v
+++ YODA/HDL/testbenches/TB_FP_MAC.v
@@ -3,11 +3,12 @@
 // FP multiplier test bench
 // Authors: Jonah Swain [SWNJON003]
 
-module TB_FP_MUL ();
+module TB_FP_MAC ();
 
 // === LOCAL PARAMETERS ===
 localparam n_A = 32'h40000000; // 2
 localparam n_B = 32'h40400000; // 3
+localparam n_C = 32'h40000000; // 2
 
 // === REGISTERS & WIRES ===
 reg CLK;
@@ -16,11 +17,13 @@ reg [31:0] A;
 reg A_valid;
 reg [31:0] B;
 reg B_valid;
+reg [31:0] C;
+reg C_valid
 wire [31:0] R;
 wire R_valid;
 
 // === MODULES ===
-FP_MUL fpm (CLK, A_valid, A, B_valid, B, R_valid, R);
+FP_MAC fpm (CLK, A_valid, A, B_valid, B, C_valid, C, R_valid, R);
 
 // === SETUP ===
 initial begin
@@ -30,14 +33,18 @@ initial begin
     CLK <= 0;
     A <= n_A;
     B <= n_B;
+    C <= n_C;
     A_valid <= 1'b1;
     B_valid <= 1'b1;
+    C_valid <= 1'b1;
     
-    repeat (20) begin // Clock generation
+    repeat (40) begin // Clock generation
     #5 CLK <= ~CLK;
     #5 CLK <= ~CLK;
     cycle <= cycle + 1;
     end
+
+
 end
 
 endmodule
